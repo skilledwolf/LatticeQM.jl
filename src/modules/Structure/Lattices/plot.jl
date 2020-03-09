925a887c586a3807bcb1936961f6452b63477b1a
Tobias Wolf <wolft@itp.phys.ethz.ch>
2020-03-09 16:26:35 +0100

    Bugfix Lattices/plot.jl

--- src/modules/Structure/Lattices/plot.jl
+++ src/modules/Structure/Lattices/plot.jl
@@ -1,16 +1,18 @@
 using Plots
 
-@recipe function f(lat0::Lattice, colors::Union{Nothing,Vector{Float64}}=nothing; filter=(), sort=false, supercell=[0:1,0:1], markercolor=:RdYlBu, clims=:auto)
+@recipe function f(lat0::Lattice, colors::Union{Nothing,Vector{Float64},String}=nothing; filter=(), sort=false, supercell=[0:1,0:1], markercolor=:RdYlBu, clims=:auto)
     # filter could be for example filter=("layer", z->z==0.0)
+
+    if isa(colors, String) # If colors is a string it is to be understood as an extraposition
+        colors = extrapositions(lat0, colors)
+    end
 
     lat = deepcopy(lat0)
     repeat!(lat, supercell)
 
+    perm = (:)
     if sort != false
         perm = sortextraposition!(lat, sort)
-        if colors != nothing
-            colors[:] = colors[perm]
-        end
     end
 
     # Plot the layers
@@ -24,7 +26,7 @@ using Plots
 
     if colors != nothing
         # Note that colors::Vector{Float64} must be provide a color for each site
-        zcolor := vcat(fill(colors, length(supercell[1])*length(supercell[2]))...)
+        zcolor := vcat(fill(colors, length(supercell[1])*length(supercell[2]))...)[perm]
         markercolor --> markercolor
         clims --> clims
     end
