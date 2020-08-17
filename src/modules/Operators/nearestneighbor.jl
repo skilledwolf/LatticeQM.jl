3663e78be31af43dc2e8c8b158e799b5ceee63bd
Tobias Wolf <wolft@itp.phys.ethz.ch>
2020-08-17 19:40:55 +0200

    Corrected Operators.nearestneighbor.

--- src/modules/Operators/nearestneighbor.jl
+++ src/modules/Operators/nearestneighbor.jl
@@ -1,8 +1,8 @@
 @legacyalias getnearestneighborhops get_NN_hops
 getnearestneighborhops(args...; kwargs...) = nearestneighbor!(Hops(), args...; kwargs...)
 
-function nearestneighbor!(hops, lat, t0=1.0; a=1.0, selector=(:))
-    t(r) = (a+0.01>r[:]>a-0.01) ? t0 : 0.0
+function nearestneighbor!(hops, lat, t0=1.0; a=1.0)
+    t(r) = (a+0.01>r>a-0.01) ? t0 : 0.0
     @scalar2vector t
 
     addhops!(hops, lat, t)
