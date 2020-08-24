b776fad5d1715a0c613d61dc26d97ded8bcf4ec0
Tobias Wolf <wolft@itp.phys.ethz.ch>
2020-08-24 18:30:48 +0200

    Added checkpoint keyword to solveselfconsinstent!.

--- src/modules/Meanfield/selfconsistent.jl
+++ src/modules/Meanfield/selfconsistent.jl
@@ -33,16 +33,25 @@ end
     at given filling (between 0 and 1). k space is discretized with the given points ks.
     returns (1) the density matrix of the meanfield (2) ground state energy of the meanfield operator (3) the chemical potential (4) convergence flag (bool) (5) error estimate
 
+    If the checkpoint keyword is set, e.g. `checkpoint="mf.jld"`, the mean field will be saved into a file at each step of the iteration,
+    allowing to interrupt and restart a long-running calculation safely. If `checkpoint` is set and the file exists, this method
+    will assume that the file contains a valid mean-field and use it as initial guess. To avoid this behavior, specify `hotstart=false`.
+
     parallel=true might help if diagonalization per k point is very time consuming
     (e.g. for twisted bilayer graphene)
     note that for small problems `parallel=true` may decrease performance (communication overhead)
 """
 function solveselfconsistent!(ρ0::AnyHops, ρ1::AnyHops, ℋ_op::Function, ℋ_scalar::Function, filling::Float64, ks::AbstractMatrix{Float64};
-    convergenceerror=false, parallel=false, iterations=500, tol=1e-7, T=0.0, format=:dense, verbose::Bool=false, kwargs...)
+    convergenceerror=false, parallel=false, checkpoint::String="", hotstart=true, iterations=500, tol=1e-7, T=0.0, format=:dense, verbose::Bool=false, kwargs...)
 
     ρ0 = Hops(δL=>Matrix(complex(m)) for (δL, m)=ρ0) # convert to dense
     ρ1 = Hops(δL=>Matrix(complex(m)) for (δL, m)=ρ1) # convert to dense
     H = Hamiltonian(Hops(), 0.0)
+
+    if checkpoint != "" && isfile(checkpoint) && hotstart
+        println("Loading checkpoint file as initial guess: $checkpoint")
+        ρ0 = JLD.load(checkpoint, "mf")
+    end
 
     function updateH!(H::Hamiltonian, ρ::AnyHops)
         verbose ? @info("Updating chemical potential for given filling.") : nothing
@@ -56,6 +65,10 @@ function solveselfconsistent!(ρ0::AnyHops, ρ1::AnyHops, ℋ_op::Function, ℋ_
 
         verbose ? @info("Updating the meanfield density matrix.") : nothing
         ϵ0 = densitymatrix!(ρ1, H.h, ks, H.μ; parallel=parallel, T=T, format=:dense) # get new meanfield and return the groundstate energy (density matrix was written to ρ1)
+
+        if checkpoint != ""
+            JLD.save(checkpoint, "mf", ρ1)
+        end
 
         ϵ0 + ℋ_scalar(ρ1) # proper ground state energy
     end
