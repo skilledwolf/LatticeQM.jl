b67d65bf9e4dad176042964318e296a5794544f2
Tobias Wolf <wolft@itp.phys.ethz.ch>
2020-09-30 10:38:01 +0200

    New method for getcappedyukawa with cellrange.

--- src/modules/Meanfield/interactions.jl
+++ src/modules/Meanfield/interactions.jl
@@ -21,9 +21,16 @@ function gethubbard(lat, neighbors=[[0;0]]; mode=:nospin, format=:auto, kwargs..
     addspin(ee_exchange, mode)
 end
 
-function getcappedyukawa(lat, neighbors=[[i;j] for i=-1:1 for j=-1:1]; cellrange=1, mode=:nospin, format=:auto, kwargs...)
+function getcappedyukawa(lat; cellrange=1, mode=:nospin, format=:auto, kwargs...)
     t(args...) = CappedYukawa(args...; kwargs...)
     ee_exchange = gethops(lat, neighbors, t; cellrange=cellrange, format=format)
+
+    addspin(ee_exchange, mode)
+end
+
+function getcappedyukawa(lat, neighbors=[[i;j] for i=-1:1 for j=-1:1]; mode=:nospin, format=:auto, kwargs...)
+    t(args...) = CappedYukawa(args...; kwargs...)
+    ee_exchange = gethops(lat, neighbors, t; format=format)
 
     addspin(ee_exchange, mode)
 end
