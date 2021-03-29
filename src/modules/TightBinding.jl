2afcf53c7ef52db43d52e804d331f5e2abd787f1
Tobias Wolf <wolft@itp.phys.ethz.ch>
2021-03-29 18:01:38 +0200

    Small clean up.

--- src/modules/TightBinding.jl
+++ src/modules/TightBinding.jl
@@ -9,14 +9,12 @@ export hopdim, addspin, addhops!, addhops, zerokey, getzero, setzero!
 export getbloch
 
 include("TightBinding/gethops.jl") # todo : move to structure
-export gethops
 
 include("TightBinding/operators.jl")
 
 include("TightBinding/superlattice.jl")
 
 include("TightBinding/neighbors.jl")
-export getneighborhops
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 end
