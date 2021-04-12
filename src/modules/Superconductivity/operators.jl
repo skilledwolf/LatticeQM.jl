17814cdfe16fe6a7ff84fc03e657e192b1badb9b
Tobias Wolf <wolft@itp.phys.ethz.ch>
2021-04-12 17:22:57 +0200

    Fixed small bug.

--- src/modules/Superconductivity/operators.jl
+++ src/modules/Superconductivity/operators.jl
@@ -4,6 +4,8 @@ using ..Operators: addchemicalpotential!
 using ..TightBinding: Hops, zerokey, hopdim
 using SparseArrays#: spzeros
 
+import ..Spectrum
+
 function Operators.addchemicalpotential!(H::BdGOperator, μ::Real)
     d = hopdim(H)
     addhops!(H, BdGOperator(Hops(zerokey(H.h)=>complex(spzeros(d,d)+μ*I))))
@@ -12,11 +14,11 @@ end
 function electron(H::BdGOperator)
     d = hopdim(H)
 
-    BdGOperator(Hops(Dict(zerokey(H)=>zero(first(values(TightBinding.getelectronsector(H))))+1.0*I)))
+    BdGOperator(Hops(Dict(zerokey(H)=>zero(first(values(Spectrum.getelectronsector(H))))+1.0*I)))
 end
 
 function Operators.localobservables(ρ::BdGOperator, lat)
-    ρ0 = getelectronsector(ρ)
+    ρ0 = Spectrum.getelectronsector(ρ)
     M = Operators.localobservables(ρ0, lat)
 
     Δ= Superconductivity.getpairingsector(ρ) * (1im*Operators.getoperator(lat, "sy"))
