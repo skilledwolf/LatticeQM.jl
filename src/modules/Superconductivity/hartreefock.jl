61055e3d72d6b23fff72e87cbf1eb1067ada8aa6
Tobias Wolf <wolft@itp.phys.ethz.ch>
2021-04-12 17:20:15 +0200

    Fixed small bug.

--- src/modules/Superconductivity/hartreefock.jl
+++ src/modules/Superconductivity/hartreefock.jl
@@ -1,13 +1,15 @@
 import ..Meanfield
 using ..Meanfield: hartreefock, hartreefock_pairing
 
+import ..Spectrum
+
 function Meanfield.hartreefock(H::BdGOperator{T}, v::AnyHops) where T<:AnyHops
 
     vMF, ΔMF, ϵMF = hartreefock_pairing(v)
 
     function H_op(ρ::BdGOperator{T}) where T<:AnyHops
 
-        ρ0 = getelectronsector(ρ)
+        ρ0 = Spectrum.getelectronsector(ρ)
         ρΔ = getpairingsector(ρ)
 
         MFOP = BdGOperator(vMF(ρ0), ΔMF(ρΔ))
@@ -17,7 +19,7 @@ function Meanfield.hartreefock(H::BdGOperator{T}, v::AnyHops) where T<:AnyHops
     end
 
     function E_scalar(ρ::BdGOperator{T}) where T<:AnyHops
-        ρ0 = getelectronsector(ρ)
+        ρ0 = Spectrum.getelectronsector(ρ)
         ρΔ = getpairingsector(ρ)
 
         ϵMF(ρ0, ρΔ)
