460c7086675f9b3265bbfdf53277999bf5a3e90b
Tobias Wolf <wolft@itp.phys.ethz.ch>
2021-08-23 01:01:09 +0200

    Changed to pyimport_conda.

--- src/modules/Utils/pycall.jl
+++ src/modules/Utils/pycall.jl
@@ -2,6 +2,6 @@
 import PyCall # adds ~ 1.5 seconds to the load time 
 
 function __init__()
-    global brentq = PyCall.pyimport("scipy.optimize").brentq
+    global brentq = PyCall.pyimport_conda("scipy.optimize", "scipy").brentq
     global cKDTree = PyCall.pyimport("scipy.spatial").cKDTree
 end
