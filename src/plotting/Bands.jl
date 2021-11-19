8c39c08561ed8b0290f1cc82b33ea46462f114a4
Tobias Wolf <wolft@itp.phys.ethz.ch>
2021-11-19 15:55:25 -0600

    Minor tweak.

--- src/plotting/Bands.jl
+++ src/plotting/Bands.jl
@@ -2,19 +2,15 @@ import Statistics: quantile
 import LatticeQM.Spectrum: BandData
 import LatticeQM.Structure.Paths: scaleticks, ticklabels
 
-@recipe function f(data::BandData, n::Integer = 1; sharpen=0.0, clims=nothing, csymmetric=true, cquantile=0.97)
+@recipe function f(data::BandData, n::Integer = 1; sharpen=0.0, climits=nothing, csymmetric=true, cquantile=0.97)
     if data.obs == nothing || n == 0
         markercolor --> :black
     else
         mycolors = data.obs[:,:,n]
         max = quantile(abs.(mycolors)[:], cquantile) # maximum(abs.(data.obs[:,:,n]))
         # max = maximum(abs.(data.obs[:,:,n]))
+        min = (csymmetric ? -max : quantile(abs.(mycolors)[:], 1-cquantile))
 
-        if csymmetric
-            min = -max
-        else
-            min = quantile(abs.(mycolors)[:], 1-cquantile)
-        end
 
         if sharpen > 0.0
             zmin = minimum(data.obs[:,:,n])
@@ -28,21 +24,14 @@ import LatticeQM.Structure.Paths: scaleticks, ticklabels
         marker_z      := transpose(mycolors)#transpose(mycolors)
         markercolor --> :diverging_bkr_55_10_c35_n256 #:RdYlBu #ColorGradient([:red,:limegreen,:blue])#
 
-        if clims==nothing
-            clims --> (x->(min-sqrt(eps()),max+sqrt(eps())))
-        else
-            clims --> (x->clims)
-        end
-
-#         max = Statistics.quantile(abs.(mycolors)[:], 0.97)
-#         clims := (-max,max)
+        clims --> ((climits==nothing) ? (min-sqrt(eps()), max+sqrt(eps())) : climits)
 
         # max = Statistics.quantile(filter(x->x>0, data.obs[:,:,n]), 0.95)
         # min = Statistics.quantile(filter(x->x<0, data.obs[:,:,n]), 0.05)
         # max = maximum(abs.([min,max]))
         zlims --> (min,max)
-
     end
+
     background_color_inside --> :lightgray
     yguide --> "Energy"
     legend := :none
